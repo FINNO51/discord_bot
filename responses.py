c57fa121fff258588472f69713a780d8f5325ca6
Jordi Conde Molina <jordicondemolina@gmail.com>
2023-06-02 00:53:16 +0200

    ofu que estrés

--- responses.py
+++ responses.py
@@ -6,18 +6,18 @@ def handle_response(message) -> str:
     p_message = message
     if 'vm.tiktok' in p_message:
         tiktok_id = re.findall('.*//vm.tiktok.com/([a-zA-Z0-9]*).*', p_message)
-        return "https://vm.vxtiktok.com/" + tiktok_id[0]
+        return "https://vm.vxtiktok.com/" + tiktok_id[0] + "/"
 
     if 'www.tiktok' in p_message and '/video/' in p_message:
         tiktok_id__ = re.findall('.*//www.tiktok.com/@([^\s]*)\s*', p_message)
-        return "https://www.vxtiktok.com/@" + tiktok_id__[0]
+        return "https://www.vxtiktok.com/@" + tiktok_id__[0] + "/"
 
     if '/twitter.com' in p_message and '/status/' in p_message:
         tweet_id = re.findall('.*//.*/.*/.*/([0-9]*).*', p_message)
         tweets = sntwitter.TwitterTweetScraper(tweet_id[0])
         for tweet in tweets.get_items():
             if 'Video' in str(tweet.media[0]):
-                return "https://vxtwitter.com/i/status/" + tweet_id[0]
+                return "https://vxtwitter.com/i/status/" + tweet_id[0] + "/"
         
         
 
